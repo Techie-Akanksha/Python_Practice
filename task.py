526e58210d551da3b984da4f711652a1af50165c
Techie-Akanksha <akankshadeshmukh.in@gmail.com>
2026-06-10 21:04:15 +0530

    Built Random Number Guessing Game

---
+++ task.py
@@ -0,0 +1,16 @@
+import random
+com = random.randint(1,100)
+chance = 0
+while True:
+    user = int(input("Guess the number between :- "))
+    chance += 1
+    if com == user:
+        print("Congratulations Buddy you guessed it right !")
+        print(f"You took {chance} Chances to correct guess !")
+        break
+    
+    elif com > user:
+        print("Wrong! Go little Upwards..")
+    
+    elif com < user:
+        print("Wrong! go little lower")
