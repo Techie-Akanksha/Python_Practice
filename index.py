f2a6a160d1b23c4422949ae4bddab4370141a7f4
Techie-Akanksha <akankshadeshmukh.in@gmail.com>
2026-06-12 20:41:56 +0530

    Practice All List Methods

--- index.py
+++ index.py
@@ -52,29 +52,42 @@
 # LIST
 # ordered, mutable, Indexing start from 0
 
-l = [10,12,12,32,43,12]
+# l = [10,12,12,32,43,12]
 # l[1] = 13
 # print(l) you can change the value as well
 
-for i in range(0,len(l)):
-    print(f"{i} = {l[i]}")
+# for i in range(0,len(l)):
+#     print(f"{i} = {l[i]}")
 
+user_list = [10,20,30,40,50]
+user_list.append(60) #append : add value at last 
 
+user_list.insert(0,0) #insert : add value at specified position 
 
+user_list.pop() #pop : Removes and returns an element from the list.removes an element by index, not by value.It returns the removed element, so you can store it in a variable. If no index is provided, pop() removes and returns the last element.
+
+user_list.remove(0) #remove : Removes the first occurrence of the specified value from the list. removes an element by value, not by index. does not return the removed value. If the value appears multiple times, only the first occurrence is deleted.
+
+user_list.sort() #sort : Sorts the original list directly.Does not return a new sorted list. By default, sorts in ascending order. Use reverse=True for descending order. works for string and numbers.
+
+user_list.reverse() #reverse : Reverses the current order of elements
+
+print(len(user_list))
+print(user_list)
 
 
 # Q1
 # Print all positive and negative elements separately.
-input= [3, -1, 4, -5, 9]
-positive = []
-negative = []
-for i in input:
-    if i > 0:
-        positive.append(i)
-    elif i < 0:
-        negative.append(i)
-print(positive)
-print(negative)
+# input= [3, -1, 4, -5, 9]
+# positive = []
+# negative = []
+# for i in input:
+#     if i > 0:
+#         positive.append(i)
+#     elif i < 0:
+#         negative.append(i)
+# print(positive)
+# print(negative)
 
 # Q2
 # Find the mean (average) of all list elements.
