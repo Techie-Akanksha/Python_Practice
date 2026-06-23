2beec1f0111ecb33f85e03b0a98606123deeb18f
Techie-Akanksha <akankshadeshmukh.in@gmail.com>
2026-06-23 13:42:57 +0530

    Practice Tuple Questions

--- index.py
+++ index.py
@@ -115,16 +115,16 @@ l = [10,12,12,32,43,12]
 # Q4
 # Find the second greatest element.
 # Input: [4, 8, 2, 9, 1]
-a = [4, 8, 2, 9, 100]
-largest = a[0]
-second_largest = a[0]
-for i in a:
-    if largest < i :
-        second_largest = largest
-        largest = i
-    elif second_largest < i and i != largest:
-        second_largest = i
-print(f"The second greatest element is {second_largest}, and the greatest element is {largest}")
+# a = [4, 8, 2, 9, 100]
+# largest = a[0]
+# second_largest = a[0]
+# for i in a:
+#     if largest < i :
+#         second_largest = largest
+#         largest = i
+#     elif second_largest < i and i != largest:
+#         second_largest = i
+# print(f"The second greatest element is {second_largest}, and the greatest element is {largest}")
 # ind= 0
 # for i in range(len(a)):
 #     if largest < a[i]:
@@ -135,27 +135,27 @@ print(f"The second greatest element is {second_largest}, and the greatest elemen
 # Q5
 # Check if the list is already sorted.
 # Input: [1, 3, 5, 7]
-a = [1, 3, 5, 7, 9]
-is_sorted = True
-for i in range(len(a) - 1):
-    if a[i] > a[i + 1]:
-        is_sorted = False
-        print("The list is not sorted.")
-        break
+# a = [1, 3, 5, 7, 9]
+# is_sorted = True
+# for i in range(len(a) - 1):
+#     if a[i] > a[i + 1]:
+#         is_sorted = False
+#         print("The list is not sorted.")
+#         break
 
-else:
-    print("The list is already sorted.")
+# else:
+#     print("The list is already sorted.")
 
 #WRITING FUNCTION FOR Q5
-def is_sorted(lst):
-    for i in range(len(lst) - 1):
-        if lst[i] > lst[i + 1]:
-            print("The list is not sorted.")
-            return False
-    print("The list is already sorted.")
-    return True
+# def is_sorted(lst):
+#     for i in range(len(lst) - 1):
+#         if lst[i] > lst[i + 1]:
+#             print("The list is not sorted.")
+#             return False
+#     print("The list is already sorted.")
+#     return True
 
-is_sorted(a)
+# is_sorted(a)
 
 
 
@@ -179,6 +179,93 @@ print(msg)      # Output: Hello, I am a tuple function!
 print(year)     # Output: 2026
 print(comment)  # Output: Python is fun!
 
+#tuple quesition
+# 1) Create a tuple containing 5 different fruits and print it.
+fruits = ("apple", "Banana", "cherry", "date", "elderberry")
+print(fruits)
+
+# 2)Create a tuple with the values (10, 20, 30, 40, 50) and:
+# Print the first element.
+# Print the last element.
+# Print the third element.
+
+numbers = (10, 20, 30, 40, 50)
+print(numbers[0])   # Output: 10
+print(numbers[-1])  # Output: 50
+print(numbers[2])   # Output: 30
+
+# 3)Print all elements using a loop.
+numbers = (1, 2, 3, 4, 5) 
+for num in numbers:
+    print(num)
+
+# 4)Find the length of:
+colors = ("red", "green", "blue", "yellow")
+print(len(colors))
+
+# 5) Check whether "apple" exists in:
+fruits = ("banana", "apple", "mango", "grapes")
+for i in fruits:
+    if i == "apple":
+        print("Found it!")
+        break
+else:
+        print("Not found.")
+
+# 6)Find the sum of all elements.
+data = (5, 10, 15, 20, 25)
+for i in data:
+    sum = 0
+    sum +=i
+print(sum)
+
+# 7)Count how many times 3 appears in:
+nums = (1, 3, 5, 3, 7, 3, 9)
+print(nums.count(3))
+
+# 8)Find the index of "Python" in:
+lang = ("Java", "C++", "Python", "JavaScript")
+print(lang.index("Python"))
+
+# 9)Convert the following list into a tuple:
+my_list = [10, 20, 30, 40]
+print(tuple(my_list))  # Output: (10, 20, 30, 40)
+
+# 10)Convert the following tuple into a list:
+my_tuple = (100, 200, 300)
+print(list(my_tuple))  # Output: [100, 200, 300]
+
+# 11)Concatenate:
+t1 = (1, 2, 3)
+t2 = (4, 5, 6)
+print(t1 + t2)  # Output: (1, 2, 3, 4, 5, 6)
+
+#12) Repeat the tuple:
+t = ("A", "B")
+print(t * 3)  # Output: ('A', 'B', 'A', 'B', 'A', 'B')
+count = 0
+# Repeat the tuple using loop
+while count < 3:
+    print(t)
+    count += 1
+# 13)into separate variables and print them.
+student = ("Rahul", 20, "Mumbai")
+name, age, city = student
+print(f"Name: {name}, Age: {age}, City: {city}")
+
+# 14)Use tuple unpacking to store:
+# First value in a
+# Last value in e
+# Remaining values in middle
+num = (1, 2, 3, 4, 5)
+a, *middle, e = num
+print(f"a: {a}, middle: {middle}, e: {e}")
+
+# 15)Swap two variables using tuple unpacking:
+x = 10
+y = 20
+x, y = y, x
+print(f"x: {x}, y: {y}")
 
 # You're studying tutorials but not building projects.
 # You're learning too many things and mastering none.
