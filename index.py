91685c2c2e9a9e5726350fd7592f059a8dfef5a9
Techie-Akanksha <akankshadeshmukh.in@gmail.com>
2026-06-27 22:09:46 +0530

    Learn File Handling

--- index.py
+++ index.py
@@ -1,59 +1,33 @@
-#Exception Handling
+# File Modes
 
-# ❌ Errors (unfixable)
-# SyntaxError — wrong syntax
-# IndentationError — bad spacing
-# TabError — mixing tabs/spaces
+# Mode	Meaning	Creates file?
+# 'r'	Read only	❌ (must exist)
+# 'w'	Write (overwrites!)	✅
+# 'a'	Append to end	✅
+# 'x'	Create (fails if exists)	✅
 
-# ✅ Exceptions (handleable!)
-# ZeroDivisionError
-# ZeroDivisionError : A ZeroDivisionError occurs when you try to divide a number by zero.
+# "x" creates the file if it's not exist
+# open("hello.txt","x")
 
-# TypeError: 
-# Ex: a= "10" b = 12 print(a+b) this will fail to give ans due to different datatype. when an operation or function is applied to an object of the wrong data type.
+#write file 
+new_file = open("hello.txt","w")
+data = input("Enter the data which you want in your file : ")
+new_file.write(data)
 
-# ValueError: Ex: int("Hello") Error occurs when a function receives an argument of the correct type but with an invalid value.
+#read the file
+read = open("hello.txt","r")
+print(read.read())
 
-# ValueError → The type is correct, but the value is invalid.
-# TypeError → The type itself is incorrect. 
+#Append the content in the file 
+file = open("hello.txt","a")
+file.write("\n This are newly added content in this file.")
 
-#FileNotFoundError
-# A FileNotFoundError is a Python exception that occurs when you try to open or access a file that does not exist at the specified path.
+# with open("te.py", "w") as f:
+#     f.write("hello python")
 
-# num = int(input("Provide no which you want to divide 10 : "))
-# try:
-#     result = 10 // num
-# except ZeroDivisionError: 
-#     print("Can't divide by zero!")
-# else: 
-#     print("Success:", result)
-# finally: 
-#     print("This always runs.")
+# with open("te.txt", "r") as f:
+#     content = f.read()
+#     print(content)
 
-try: 
-    num = int(input("Enter Positive number : "))
-    if 0 > num :
-        raise ValueError("Negative value are not allowed.")
-    
-    result = 100 // num 
-
-except ValueError as e:
-    print("ValueError :",e)
-
-except ZeroDivisionError:
-    print("You cannot divide by zero")
-
-else:
-    print("Division Result: ", result)
-
-finally:
-    print("Program Execution Completed")
-
-# try values : 10, 0, -5, abc
-
-# Keyword	Code	Purpose
-# try	try:	Wraps code that might cause an exception.
-# raise	raise ValueError("Negative numbers are not allowed.")	Manually throws an exception if the number is negative.
-# except	except ValueError..., except ZeroDivisionError...	Catches and handles specific exceptions.
-# else	print("Division Result:", result)	Runs only if no exception occurs.
-# finally	print("Program execution completed.")	Always runs, whether an exception occurs or not.
+# with open("te.txt","a") as f:
+#     f.write("\nAdded a new line")
