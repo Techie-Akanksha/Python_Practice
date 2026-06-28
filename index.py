7800eef56cb9339a34cfdd3c58f87608d3b8d5be
Techie-Akanksha <akankshadeshmukh.in@gmail.com>
2026-06-28 22:57:44 +0530

    Completed File Handling Practice Project

--- index.py
+++ index.py
@@ -14,15 +14,78 @@ def createfile():
             print("Error your file is already exists")
     
     except Exception as err:
-        print(f"an error occured due as {err}")
+        print(f"an error occured as {err}")
 
 
 def updatefile():
-    pass
+    try : 
+        filename = input("Enter file name you want to update : ")
+        path = Path(filename)
+        if path.exists():
+            print("Operations")
+            print("1. Renaming the file")
+            print("2. Append the content")
+            print("3. Overwriting the file")
+
+            choice = int(input("Enter your operation :- "))
+
+            if choice == 1:
+                new_filename = input("Enter new file name :- ")
+                new_path = Path(new_filename)
+                if not new_path.exists():
+                    path.rename(new_path)
+                    print("Renamed Successfully")
+                else:
+                    print("Error this file name already exists")
+        
+            elif choice == 2:
+                with open(path,"a") as fs:
+                    content = input("What do you want to append : ")
+                    fs.write("\n" + content)
+                print("Content append successfully")
+
+            elif choice == 3:
+                with open(path,"w") as fs:
+                    content = input("Enter content to overwrite in file : ")    
+                    fs.write(content)
+                print("Successfully overwritten")
+
+            else : 
+                print("Error: Select proper action")
+
+        else:                        
+            print("Error file not exists")
+
+    except Exception as err:
+        print(f"an error occured as {err}")  
+
+
 def readfile():
-    pass
+    try : 
+        filename = input("Enter your file name : ")
+        path = Path(filename)
+        if path.exists():
+            with open(path, "r") as fs:
+                content = fs.read()
+                print(f"Your file content is \n {content}")
+        else:
+            print("Error no such file exists")
+
+    except Exception as err:
+        print(f"an error occured as {err}")
+
 def deletefile():
-    pass
+    try:
+        filename = input("Enter your file name : ")
+        path = Path(filename)
+        if path.exists():
+            path.unlink()
+            print("File deleted successfully")
+        else:
+            print("Error there is no such file exists")
+
+    except Exception as err:
+        print(f"an error occured as {err} ")
 
 print("press 1 for creating a new file")
 print("press 2 for updating an existing file")
@@ -40,5 +103,5 @@ elif response == 3:
 elif response == 4:
     deletefile()
 else:
-    print("Error: Your are not selecting proper action")
+    print("Error: Select proper action")
 
