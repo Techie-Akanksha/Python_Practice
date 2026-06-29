1ad88b6ff64b0521c8c429be17eb40f6188f80fd
Techie-Akanksha <akankshadeshmukh.in@gmail.com>
2026-06-29 19:01:13 +0530

    Some changes done in project

--- index.py
+++ index.py
@@ -1,108 +1,147 @@
 from pathlib import Path
 
+# Function to create file
 def create_file():
     try: 
         filename = input("Enter your file name : ")
         path = Path(filename)
         if not path.exists():
-            with open(path, "w") as f:
+            # encoding="utf-8"avoids issues with non-English text.
+            with open(path, "w", encoding="utf-8") as f:
                 data = input("Enter your content inside your file : ")
                 f.write(data)
             print("File created successfully")
+        elif not filename.strip():
+            print("Filename cannot be empty. Try again.")
         else:
             print("Error your file is already exists")
     
     except Exception as err:
         print(f"an error occured as {err}")
 
-
+# Function to update file
 def update_file():
     try : 
         filename = input("Enter file name you want to update : ")
         path = Path(filename)
-        if path.exists():
+
+        if not filename.strip():
+            print("Please enter filename again.")
+            
+
+        elif path.exists():
             print("Operations")
             print("1. Renaming the file")
             print("2. Append the content")
             print("3. Overwriting the file")
 
-            choice = int(input("Enter your operation :- "))
+            try:
+                choice = int(input("Enter your operation :- "))
 
-            if choice == 1:
-                new_filename = input("Enter new file name :- ")
-                new_path = Path(new_filename)
-                if not new_path.exists():
-                    path.rename(new_path)
-                    print("Renamed Successfully")
+                if choice == 1:
+                    new_filename = input("Enter new file name :- ")
+                    new_path = Path(new_filename)
+                    if not new_path.exists():
+                        path.rename(new_path)
+                        print("Renamed Successfully")
+                    else:
+                        print("Error this file name already exists")
+            
+                elif choice == 2:
+                    with open(path,"a") as fs:
+                        content = input("What do you want to append : ")
+                        fs.write("\n" + content)
+                    print("Content append successfully")
+
+                elif choice == 3:
+                    # encoding="utf-8"avoids issues with non-English text.
+                    with open(path,"w",encoding="utf-8") as fs:
+                        content = input("Enter content to overwrite in file : ")    
+                        fs.write(content)
+                    print("Successfully overwritten")
                 else:
-                    print("Error this file name already exists")
-        
-            elif choice == 2:
-                with open(path,"a") as fs:
-                    content = input("What do you want to append : ")
-                    fs.write("\n" + content)
-                print("Content append successfully")
+                    print("Please Select proper action")
 
-            elif choice == 3:
-                with open(path,"w") as fs:
-                    content = input("Enter content to overwrite in file : ")    
-                    fs.write(content)
-                print("Successfully overwritten")
-
-            else : 
-                print("Error: Select proper action")
+            except ValueError as err:
+                print("ValueError:",err)
 
         else:                        
-            print("Error file not exists")
+            print("Error: File doesn't exist.")
+            tocreate = input("Create it? (y/n) : ")
+            if tocreate.lower() == "y":
+                create_file()
+            else:
+                print("Get your response : File doesn't exist")
+
 
     except Exception as err:
         print(f"an error occured as {err}")  
 
-
+# Function to read the file
 def read_file():
     try : 
         filename = input("Enter your file name : ")
         path = Path(filename)
-        if path.exists():
+        if not filename.strip():
+            print("Please enter filename again.")
+
+        elif path.exists():
             with open(path, "r") as fs:
                 content = fs.read()
-                print(f"Your file content is \n {content}")
+                print(f"Your file content is \n{content}")
+
         else:
-            print("Error no such file exists")
+            print("Error: No such file exists")
 
     except Exception as err:
         print(f"an error occured as {err}")
 
+# Function to delete file
 def delete_file():
     try:
         filename = input("Enter your file name : ")
         path = Path(filename)
-        if path.exists():
+        if not filename.strip():
+            print("Invalid Action. Enter your file name.")
+
+        elif path.exists():
             confirm = input("Deleting this file? (y/n): ")
+
+            # Taking confirmation from user before deleting file. 
             if confirm.lower() == "y":
                 path.unlink()
                 print("File deleted successfully")
+            else :
+                print("Thanks for confirming! Your file is not deleted.")
+
         else:
-            print("Error there is no such file exists")
+            print("Error: There is no such file exists")
 
     except Exception as err:
         print(f"an error occured as {err} ")
 
-print("press 1 for creating a new file")
-print("press 2 for updating an existing file")
-print("press 3 for reading an existing file")
-print("press 4 for deleting a file")
 
-response = int(input("Enter your response here :- "))
 
+print("""1. Create
+2. Update
+3. Read
+4. Delete""")
+    
+try : 
+    response = int(input("Enter your response here :- "))
+except ValueError:
+    print("Please enter numbers only.")
+
+    
 if response == 1:
-    create_file()
+        create_file()
 elif response == 2:
-    update_file()
+        update_file()
 elif response == 3:
-    read_file()
+        read_file()
 elif response == 4:
-    delete_file()
+        delete_file()
+
 else:
-    print("Error: Select proper action")
+        print("Error: Select proper action")
 
