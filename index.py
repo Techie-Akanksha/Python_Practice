ff8d067475f9780d9f2586d97379911dfce7a8fe
Techie-Akanksha <akankshadeshmukh.in@gmail.com>
2026-07-06 20:51:06 +0530

    Learn Inheritance Topic from OOPS concept

--- index.py
+++ index.py
@@ -1,69 +1,130 @@
 # OOPS CONCEPT
 
-# OBJECT and INsTANCE difference:
-# An object is the actual entity created from a class. An instance means that the object belongs to a particular class. In Python, the terms object and instance are generally used interchangeably. For example, if dog = Animal(), then dog is an object and also an instance of the Animal class.
+# Inheritance
+# Inheritance is a fundamental concept in object-oriented programming (OOP) that allows a class (called a child or subclass) to inherit properties and behaviors (methods) from another class (called a parent or superclass). This promotes code reusability and establishes a hierarchical relationship between classes.  
+
+# Benefits of using inheritance is :
+# 1. Code reusability
+# 2. Organized structure
+# 3. Easy to maintain and extend 
+
+class Parent:
+    def speak(self):
+        print("I am the parent class.")
+
+class child(Parent):
+    pass
+
+child_instance = child()
+child_instance.speak()  # Output: I am the parent class.
+
+# Constructor in Inheritance
+
+# when you create a parent class with a constructor function inside it and then this class is inherited by another class then constuctor function of parent class will work for the child class as well.
+
+class parent:
+    def __init__(self, name):
+        self.name = name
+
+class child(parent):
+    def __init__(self, name, age):
+        super().__init__(name)
+        self.age = age
+
+    def display(self):
+        print(f"My name is {self.name} and I am {self.age} years old.")
+
+# If you need a new parameter in your child class you have to create a constructor function for your child class but the parameters that can be initialized in the parent class will be initialized using the super() function. Super function will target the parent class.
+
+child_instance = child("John",19)
+child_instance.display()  # Output: My name is John.
+
+#Types of Instance 
+
+# 1. Single Level Inheritance
+
+# Single Inheritance means one child class inherits the properties and methods of one parent class. This allows the child class to reuse the parent's code and also add its own features.
 
 class Animal:
-    kingdom = "Animalia"
 
     def __init__(self, name):
         self.name = name
 
-    def speak(self):
-        print(f"{self.name} is speaking")
-        
-    @classmethod
-    def show_kingdom(cls):
-        print(cls.kingdom)
-
-    @staticmethod
-    def add(a, b):
-        return a + b
-    
-#Instance method
-dog = Animal("Dog")
-dog.speak()
-
-# Instance method works with instance variables and requires an object to call it. It takes self as the first parameter.
-
-#class method
-Animal.show_kingdom()
-
-# Class methods work with class variables. They take cls as the first parameter and are called using the class.
-
-#Static method
-Animal.add(5, 10)
-
-# Static methods neither access instance variables nor class variables. They behave like normal utility functions inside the class.
-
-class employee:
-    company_name = "ABC Pvt Ltd"
-    def __init__(self, name, salary):
-        self.name = name
-        self.salary = salary
-        
-    def show_salary(self):
-        print(f"{self.name}'s salary is: {self.salary}")
-    
-    @classmethod
-    def show_company_name(cls):
-        print(f"Company name is: {cls.company_name}")
-    
-    @staticmethod
-    def calculate_gst(amount):
-        gst = amount * 0.18
-        print(f"GST on ₹{amount} is ₹{gst}")
+    def eat(self):
+        print(f"{self.name} is eating.")
 
 
-rahul = employee("Rahul", 50000)
-amit = employee("Amit", 60000)
-priya = employee("Priya", 70000)
+# Child Class
+class Dog(Animal):
 
-rahul.show_salary()
-amit.show_salary()
-priya.show_salary()
+    def bark(self):
+        print(f"{self.name} is barking.")
 
-employee.show_company_name()
-employee.calculate_gst(50000)
-employee.calculate_gst(60000)
-employee.calculate_gst(70000)
+
+# Creating Object
+dog1 = Dog("Tommy")
+
+# Method inherited from Animal
+dog1.eat()
+
+# Method of Dog class
+dog1.bark()
+
+# 2. MultiLevel Inheritance
+
+# Multilevel Inheritance means a class inherits from another class, and that class itself inherits from another class. This forms a chain of inheritance, allowing the last child class to access features from all its ancestor classes.
+
+class LivingThing:
+
+    def breathe(self):
+        print("Living things breathe.")
+
+
+class Animal(LivingThing):
+
+    def eat(self):
+        print("Animals eat food.")
+
+
+class Dog(Animal):
+
+    def bark(self):
+        print("Dog barks.")
+
+
+# Creating object of Dog
+dog = Dog()
+
+# Dog can access methods from all levels
+dog.breathe()   # From LivingThing
+dog.eat()       # From Animal
+dog.bark()      # From Dog
+
+# 3. Multiple Inheritance
+
+# Multiple inheritance is a feature in OOP where a child class inherits from more than one parent class, allowing it to access attributes and methods of all parent classes.
+
+# MRO (Method Resolution Order) : Left to Right priority
+# Agar dono parent class me same method name ho:
+
+class Father:
+    def driving(self):
+        print("Father: Knows driving")
+
+
+class Mother:
+    def cooking(self):
+        print("Mother: Knows cooking")
+
+
+class Child(Father, Mother):
+    def gaming(self):
+        print("Child: Knows gaming")
+
+
+# Object
+c = Child()
+
+c.driving()   # from Father
+c.cooking()   # from Mother
+c.gaming()    # from Child
