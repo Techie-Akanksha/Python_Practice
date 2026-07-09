5c468063835d321d9385c035d80c8058e4d59e79
Techie-Akanksha <akankshadeshmukh.in@gmail.com>
2026-07-09 20:19:38 +0530

    Mini challenge to practice OOPS Concepts

--- index.py
+++ index.py
@@ -1,44 +1,29 @@
 # OOPS CONCEPT
 
-#Inheritance Pratice Example 
+class BankAccount:
+    def __init__(self,owner, balance):
+        self.owner = owner
+        self.balance = balance
 
+    def deposit(self, amount):
+        if amount > 0:
+            self.balance += amount
+            print("Deposit successful")
+        else:
+            print("Invalid amount")
 
-class Person:
-    def __init__(self, name, age):
-        self.name = name 
-        self.age = age
+    def withdraw(self,amount):
+        if self.balance >= amount and amount > 0:
+            self.balance -= amount
+            print("Withdraw successful")
+        else:
+            print("Insufficient Balance")
+        
+    def show_balance(self):
+        return self.balance
 
-    def display_info(self):
-        print(f"Person's name is {self.name}\nAge is {self.age} years old.")
-
-class Student(Person):
-    def __init__(self, name, age, Student_id, grade):
-        super().__init__(name, age)
-        self.Student_id = Student_id
-        self.grade = grade
-
-    def display_info(self):
-        super().display_info()
-        print(f"Student ID is {self.Student_id}")
-        print(f"Grade is {self.grade}")
-
-class Teacher(Person):
-    def __init__(self, name, age,subject, salary):
-        super().__init__(name, age)
-        self.subject = subject
-        self.salary = salary
-    def display_info(self):
-        super().display_info()
-        print(f"Subject is {self.subject}")
-        print(f"Salary is {self.salary}")
-
-
-one_student = Student("Alice", 20, "S101", "A")
-one_teacher = Teacher("Mr. John", 40, "Mathematics", 65000)
-print("----- Student Details -----")
-one_student.display_info()
-
-print()
-
-print("----- Teacher Details -----")
-one_teacher.display_info()
+account1 = BankAccount("Rahul", 1000)
+balance = account1.show_balance()
+account1.deposit(500)
+account1.withdraw(-1500)
+print(balance)
